17fd7453f6604305a410dedba034634a26ac41f5
Mubin <kmhmubin@gmail.com>
2021-07-10 22:25:58 +0600

    style #10: add bootstrap bundles

--- express-server/index.js
+++ express-server/index.js
@@ -1,13 +1,31 @@
 // require express
 const express = require("express");
 const app = express();
+const path = require("path");
+const redditData = require("./data.json");
 
 // view engine setup
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+// public folder
+app.use(express.static(path.join(__dirname, "/public")));
 
 // root route
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("home");
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  const data = redditData[subreddit];
+  if (data) {
+    res.render("subreddit", {
+      ...data,
+    });
+  } else {
+    res.render("404.ejs", { subreddit });
+  }
 });
 
 app.listen(3000, () => {
