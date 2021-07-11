fff70e64fb01639ea4ae3507cb79df3d2f31f22f
Mubin <kmhmubin@gmail.com>
2021-07-11 21:38:00 +0600

    feat #10: REST API using comments

---
+++ REST API/index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// dummy comments
+const comments = [
+  {
+    username: "Todd",
+    comment: "LOL that is so funny!",
+  },
+  {
+    username: "Skyler",
+    comment: "I like to go birdwathching with my cat",
+  },
+  {
+    username: "Sk8erBoi",
+    comment: "Plz delete your account, Todd",
+  },
+  {
+    username: "onlysaysMeoww",
+    comment: "meoww meoww meoww",
+  },
+];
+
+// base request for comments url
+app.get("/comments", (req, res) => {
+  res.render("comments/index", { comments });
+});
+
+// base request for new comment url
+app.get("/comments/new", (req, res) => {
+  res.render("comments/new");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on PORT 3000");
+});
