af3ec37a96b50bbf6c5d2fc7b33a33ac62d0241f
Mubin <kmhmubin@gmail.com>
2021-07-14 03:11:04 +0600

    feat #11: add products page

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 // import product model
 const Product = require("./models/product");
 
-// Database connection
+// Database connection using mongoose
 mongoose
   .connect("mongodb://localhost:27017/farmStand", {
     useNewUrlParser: true,
@@ -22,8 +22,23 @@ mongoose
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send("index nigga");
+// Set up routes
+app.get("/", async (req, res) => {
+  const products = await Product.find({});
+  console.log("Products:", products);
+  res.send("Show All Products");
+});
+
+// set up RESTful routes for product
+app.get("/products", async (req, res) => {
+  const products = await Product.find({});
+  res.render("products/index", { products });
+});
+
+// set up RESTful routes for product by id
+app.get("/products/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  res.render("products/show", { product });
 });
 
 // app listen port 3000
