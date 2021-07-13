5e2b0fc6d8d22adea8aa1e05e7cd2695467a577c
Mubin <kmhmubin@gmail.com>
2021-07-14 02:24:50 +0600

    inti #11: FarmHouse Sell Website with express and mongoose
    - connect express server with mongoose
    - create a product model

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -1,14 +1,32 @@
+const express = require("express");
+const app = express();
+const path = require("path");
 const mongoose = require("mongoose");
 
-// connect to mongodb using mongoose
+// import product model
+const Product = require("./models/product");
+
+// Database connection
 mongoose
-  .connect("mongodb://localhost:27017/movieApp", {
+  .connect("mongodb://localhost:27017/farmStand", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("Connected to mongodb");
+    console.log("Connected to database");
   })
   .catch((err) => {
-    console.log("Error connecting to mongodb", err);
+    console.log("Error connecting to database:", err);
   });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.send("index nigga");
+});
+
+// app listen port 3000
+app.listen(3000, () => {
+  console.log("app listen port 3000");
+});
---
+++ MongoDB/models/product.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+// product schema
+const ProductSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true, min: 0 },
+  category: {
+    type: String,
+    required: true,
+    enum: ["fruit", "vegetable", "dairy"],
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+// Product model
+const Product = mongoose.model("Product", ProductSchema);
+
+// export the model
+module.exports = Product;
