e086681b07bafac852b68e117472d15aac7cda93
Mubin <kmhmubin@gmail.com>
2021-07-13 12:20:21 +0600

    feat #11: connecting to mongodb using mongoose

---
+++ MongoDB/index.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+// connect to mongodb using mongoose
+mongoose
+  .connect("mongodb://localhost:27017/movieApp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to mongodb");
+  })
+  .catch((err) => {
+    console.log("Error connecting to mongodb", err);
+  });
