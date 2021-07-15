bff2d7605e99440840be7bbec237b0eae31dfe93
Mubin <kmhmubin@gmail.com>
2021-07-15 21:33:05 +0600

    feat #12: connect to mongodb using mongoose

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const path = require("path");
+
+// Connect to database
+mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+// Connect to mongoose db
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Connected to mongoDB");
+});
 
 // set up the view engine
 app.set("view engine", "ejs");
