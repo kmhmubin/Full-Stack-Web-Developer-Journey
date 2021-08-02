3887dbc2f2b6be4cf6d2cb1db09799baccc93be5
Mubin <kmhmubin@gmail.com>
2021-08-02 23:16:06 +0600

    seed #12: add a default author object id

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -24,6 +24,7 @@ const seedDB = async () => {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: "610800db3d9d6322d4e54400",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: "https://source.unsplash.com/collection/10586465",
