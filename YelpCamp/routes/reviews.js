da362c44bb4e81282a36cf40cfc95c0f91bd84a0
Mubin <kmhmubin@gmail.com>
2021-08-03 00:30:37 +0600

    refactor #12: review router

--- YelpCamp/routes/reviews.js
+++ YelpCamp/routes/reviews.js
@@ -1,32 +1,23 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
+
 const Campground = require("../models/campground");
 const Review = require("../models/review");
-
-const { reviewSchema } = require("../schemas.js");
 
 const ExpressError = require("../utils/ExpressError");
 const catchAsync = require("../utils/catchAsync");
 
-// backend middleware Review form validation
-const validateReview = (req, res, next) => {
-  const { error } = reviewSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(", ");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
-
 // set up the RESTful route for campground review
 router.post(
   "/",
+  isLoggedIn,
   validateReview,
   catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
@@ -38,13 +29,16 @@ router.post(
 // set up the RESTful route for campground review delete
 router.delete(
   "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = await Review.findByIdAndDelete(req.params.reviewId);
-    campground.reviews.pull(review);
-    await campground.save();
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, {
+      $pull: { reviews: { _id: reviewId } },
+    });
+    await Review.findByIdAndDelete(reviewId);
     req.flash("success", "Review deleted!");
-    res.redirect(`/campgrounds/${campground._id}`);
+    res.redirect(`/campgrounds/${id}`);
   })
 );
 
