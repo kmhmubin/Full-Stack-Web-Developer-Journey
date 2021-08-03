b90fe5c88c9766a7ad9f65da575b075bc5ebdc45
Mubin <kmhmubin@gmail.com>
2021-08-03 17:25:02 +0600

    feat #12: add RESTful review route using controller

--- YelpCamp/routes/reviews.js
+++ YelpCamp/routes/reviews.js
@@ -6,40 +6,25 @@ const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 const Campground = require("../models/campground");
 const Review = require("../models/review");
 
+const reviewsController = require("../controllers/reviews");
+
 const ExpressError = require("../utils/ExpressError");
 const catchAsync = require("../utils/catchAsync");
 
-// set up the RESTful route for campground review
+// RESTful route pattern for creating review
 router.post(
   "/",
   isLoggedIn,
   validateReview,
-  catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash("success", "Review created!");
-    res.redirect(`/campgrounds/${campground._id}`);
-  })
+  catchAsync(reviewsController.createReview)
 );
 
-// set up the RESTful route for campground review delete
+// RESTful route for delete review
 router.delete(
   "/:reviewId",
   isLoggedIn,
   isReviewAuthor,
-  catchAsync(async (req, res) => {
-    const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, {
-      $pull: { reviews: { _id: reviewId } },
-    });
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Review deleted!");
-    res.redirect(`/campgrounds/${id}`);
-  })
+  catchAsync(reviewsController.deleteReview)
 );
 
 // export the router
