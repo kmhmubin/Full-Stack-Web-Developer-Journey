4993d96f2ebb2d5e64185946330d2f1d7e0a9648
Mubin <kmhmubin@gmail.com>
2021-08-04 20:03:14 +0600

    feat #12: add geography data in campground model

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -18,6 +18,17 @@ imageSchema.virtual("thumbnail").get(function () {
 const CampgroundSchema = new Schema({
   title: String,
   images: [imageSchema],
+  geometry: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
   price: Number,
   description: String,
   location: String,
